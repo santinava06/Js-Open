cdbd3fa3bf04017a9d35ebe4e7b57d7e882593b3
Santiago <sncarp2003@gmail.com>
2023-04-15 20:09:37 -0300

    Segundo

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("Hola, Mundo!");
+// 1)
+let nombre = "Santiago Navarro";
+let edad = 19;
+let desarrolador = true;
+let fecha = new Date('august 7 2003');
+let librofav = ['harry potter y la piedra filosofal', 'J.K. Rowling', '26 de junio pero en 1997.', 'https://ele.chaco.gob.ar/mod/resource/view.php?id=151502']
