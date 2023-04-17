0e4abcb6f8edef093911dfa38c20af44ddfe758b
Santiago <sncarp2003@gmail.com>
2023-04-17 15:55:26 -0300

    Tercer

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-console.log("Hola, Mundo!");
+/*console.log("Hola, Mundo!");
 // 1)
 let nombre = "Santiago Navarro";
 let edad = 19;
 let desarrolador = true;
 let fecha = new Date('august 7 2003');
 let librofav = ['harry potter y la piedra filosofal', 'J.K. Rowling', '26 de junio pero en 1997.', 'https://ele.chaco.gob.ar/mod/resource/view.php?id=151502']
+*/
+
+//2)
+
+/*let factorial = 1;
+for (let i = 1; i <= 10; i++) {
+  factorial *= i;
+}
+console.log(factorial);*/
+
+/*let factorial2 = 1;
+let i = 1;
+while (i <= 10) {
+  factorial2 *= i;
+  i++;
+}
+console.log(factorial2);*/
+
+/*let factorial3 = 1;
+let i = 1;
+while (true) {
+  factorial3 *= i;
+  i++;
+  if (i > 10) {
+    break;
+  }
+}
+console.log(factorial3);*/
